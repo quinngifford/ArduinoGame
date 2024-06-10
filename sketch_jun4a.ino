4f9fdf08bfbf843f45ffba16692116f738b575a7
Quinn Gifford <155132289+quinngifford@users.noreply.github.com>
2024-06-10 13:14:28 -0700

    Add files via upload

--- sketch_jun4a.ino
+++ sketch_jun4a.ino
@@ -7,18 +7,20 @@ const int pinS = 7;
 volatile bool stateL = false;
 volatile bool stateR = false;
 volatile bool stateS = false;
+
 AsyncDelay timer;
 AsyncDelay beep;
+
 float midi[127];
 int A_four = 440;
 
-int totalmillis = 0;
 int level = 0;
 int points = 0;
 int c = 0;
 int guess[] = {0,0,0};
 int color[] = {0,0,0};
 int increment = 1;
+int secs;
 
 void interruptL(){
   stateL = true;
@@ -34,36 +36,42 @@ void interruptS(){
 void setup() {
   CircuitPlayground.begin();
   Serial.begin(9600);
-  attachInterrupt(digitalPinToInterrupt(pinL), interruptL, RISING);
-  attachInterrupt(digitalPinToInterrupt(pinR), interruptR, RISING);
+  attachInterrupt(digitalPinToInterrupt(pinL), interruptL, FALLING);
+  attachInterrupt(digitalPinToInterrupt(pinR), interruptR, FALLING);
   attachInterrupt(digitalPinToInterrupt(pinS), interruptS, CHANGE);
   timer.start(20000, AsyncDelay::MILLIS);
   beep.start(2000, AsyncDelay::MILLIS);
   randomColors();
+  generateMIDI();
 }
 
 void loop() {
   if(timer.isExpired()){
-    Serial.println("Level complete");
+    Serial.println();Serial.print("Level ");Serial.print(level+1);Serial.print("/");Serial.print(8);Serial.println(" complete! ");
     level++;
     if(level > 7){
+      Serial.print("FINAL SCORE: "); Serial.println(points);Serial.println();
       level = 0;
+      points = 0;
     }
-    points += abs(color[0]-guess[0])+abs(color[1]-guess[1])+abs(color[2]-guess[2]);
-    guess[0] = 0;guess[1] = 0;guess[2] = 0;color[0] = 0;color[1] = 0;color[2] = 0;
-    randomColors();
-    int secs = (20-(level*2))*1000;
-    totalmillis+=secs;
+    int accuracy = abs(color[0]-guess[0])+abs(color[1]-guess[1])+abs(color[2]-guess[2]);
+    points+= accuracy;
+    Serial.print("Color: ");Serial.print(color[0]);Serial.print(", ");Serial.print(color[1]);Serial.print(", ");Serial.print(color[2]);Serial.println(", ");
+    Serial.print("Guess: ");Serial.print(guess[0]);Serial.print(", ");Serial.print(guess[1]);Serial.print(", ");Serial.print(guess[2]);Serial.println(", ");
+    Serial.print("Error: "); Serial.print(accuracy); Serial.print(" Total Error: "); Serial.println(points);Serial.println();
+    secs = (20-(level*2))*1000;
     timer.start(secs, AsyncDelay::MILLIS);
-    Serial.println("Next Level");
-    c = 0;
+    Serial.print("Level ");
+    Serial.println(level);
+    guess[0] = 0;guess[1] = 0;guess[2] = 0;
+    randomColors();
   }
   else
   {
     if(beep.isExpired()){
-      float beepDelay = (millis()-totalmillis)*0.1;
+      float beepDelay = (0-timer.getDuration())*0.1;
       beep.start(beepDelay,AsyncDelay::MILLIS);
-      CircuitPlayground.playTone(midi[60], 100);
+      CircuitPlayground.playTone(midi[60], 50);
     }
     if(stateS){
       if(increment == 1){
@@ -72,33 +80,62 @@ void loop() {
       else{
         increment = 1;
       }
+      Serial.print("Increment: ");
+      Serial.println(increment);
       stateS = false;
     }
-    if(stateL&!stateR){
-      guess[c] -= increment;
-      stateL = false;
-    }
-    else if(stateR&!stateL){
-      guess[c] += increment;
-      stateR = false;
-    }
-    else if(stateR&stateL){
+    if(stateL){
       c++;
       if(c>2){
         c=0;
       }
-      stateR = false;
+      String cola;
+      switch(c){
+        case 0:
+          cola = "Red: ";
+          break;
+        case 1:
+          cola = "Green: ";
+          break;
+        case 2:
+          cola = "Blue: ";
+          break;
+      }
+      Serial.print(cola);
+      Serial.println(guess[c]);
       stateL = false;
     }
-
+    else if(stateR){
+      if(guess[c]+increment<=255){
+        guess[c] += increment;
+      }
+      else{
+        guess[c]=255;
+      }
+      String cola;
+      switch(c){
+        case 0:
+          cola = "Red: ";
+          break;
+        case 1:
+          cola = "Green: ";
+          break;
+        case 2:
+          cola = "Blue: ";
+          break;
+      }
+      Serial.print(cola);
+      Serial.println(guess[c]);
+      stateR = false;
+    }
   }
 }
 void randomColors(){
-  int r1 = random(255);
-  int r2 = random(255);
-  int r3 = random(255);
+  color[0] = random(255);
+  color[1] = random(255);
+  color[2] = random(255);
   for (int i=0; i < 10; i++){
-      CircuitPlayground.setPixelColor(i, r1,r2,r3);
+      CircuitPlayground.setPixelColor(i, color[0],color[1],color[2]);
   }
 }
 
